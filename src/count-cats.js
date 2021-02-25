cf7580f38da473e2683f7550a745783009e18150
gtycbjyth <kovaltru@gmail.com>
2021-02-25 22:35:46 +0300

    init: strart task. add: countCats()

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,14 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
+module.exports = function countCats(matrix) {
+  // throw new CustomError('Not implemented');
   // remove line with error and write your code here
+  let counter = 0;
+  for (let i = 0; i < matrix.length; i++){
+      let test = matrix[i]
+    for ( let j = 0; j < test.length; j++){
+      test[j] === '^^'? counter++: 0;
+    }
+  }
+return counter
 };
