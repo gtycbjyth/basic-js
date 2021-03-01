c8942bee7972a47d22ae2c486631bc0d61c31423
gtycbjyth <kovaltru@gmail.com>
2021-03-01 13:29:55 +0300

    add:dream-team()

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,19 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  // console.log(Array.isArray(members));
+  // console.log(typeof(member));
+  console.log(`member - ${members}`);
+if (!Array.isArray(members)){
+  return false
+}
+
+
+  let res = members.reduce((acc, item)=>
+    typeof item === 'string' ? acc += item.trim()[0].toUpperCase() : acc
+  ,'');
+
+  res = Array.isArray(members) ? res.split('').sort().join('') : false ;
+  return res
+
 };
